1d5d1ab111d610ac4165b00e35297a1d4a32f04e
maksonday <55452265+maksonday@users.noreply.github.com>
2019-09-22 19:26:08 +0300

    Update hw1_subarr.py

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -6,11 +6,11 @@ def find_subarr(input_lst, num):
     a = {}
     summa = 0
     for i, val in enumerate(input_lst):
-    	summa += val
-    	if summa - num in a:
-    		return(a[summa - num], i)
-    	elif val == num:
-    		return(i, i)
-    	else:
-    		a[summa - val] = i
+        summa += val
+        if summa - num in a:
+            return(a[summa - num], i)
+        elif val == num:
+            return(i, i)
+        else:
+            a[summa - val] = i
     return ()
